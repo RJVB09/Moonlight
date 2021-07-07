faaca0ef99f1131a5d69844a33b5ed1b3dab96fc
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2021-07-07 13:26:58 +0200

    Starting actual dev
    Added math class and some experimental calculations

---
+++ Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+using UnityEngine;
+
+namespace Moonlight
+{
+    class Math
+    {
+        public static double PythagoreanV3(Vector3 v3a, Vector3 v3b) 
+        {
+            double d = Sqrt(Pow((v3a.x - v3b.x), 2) + Pow((v3a.y - v3b.y), 2) + Pow((v3a.z - v3b.z), 2));
+            return d;
+        }
+        public static double PythagoreanV2(Vector2 v3a, Vector2 v3b)
+        {
+            double d = Sqrt(Pow((v3a.x - v3b.x), 2) + Pow((v3a.y - v3b.y), 2));
+            return d;
+        }
+    }
+}
--- Moonlight.cs
+++ Moonlight.cs
@@ -4,7 +4,9 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Math;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 using System.Reflection;
 
@@ -16,18 +18,20 @@ namespace Moonlight
         GameObject go;
         Light light;
         CelestialBody planet;
-
         System.Random rnd = new System.Random();
+
+        public static Vector3
         public void Start()
         {
-            go = new GameObject();
-            light = go.AddComponent<Light>();
-            light.type = LightType.Directional;
-            light.shadows = LightShadows.Hard;
-            StartCoroutine(SlowUpdate());
-            go.transform.position = Vector3.zero;
+            //go = new GameObject();
+            //light = go.AddComponent<Light>();
+            //light.type = LightType.Directional;
+            //light.shadows = LightShadows.Hard;
+            //StartCoroutine(SlowUpdate());
+            //go.transform.position = Vector3.zero;
 
             planet = FlightGlobals.GetBodyByName("Kerbin");
+            string curentScene = SceneManager.GetActiveScene().name;
         }
         public IEnumerator SlowUpdate()
         { 
@@ -39,10 +43,25 @@ namespace Moonlight
 
         public void Update()
         {
-            light.intensity = (float)rnd.Next(1, 4);
-            light.color = new Color((float)rnd.NextDouble(), (float)rnd.NextDouble(), (float)rnd.NextDouble(), 1.0F);
-            go.transform.rotation *= Quaternion.Euler(0.0F, -90 * Time.deltaTime, 0.0F);
-            //Debug.Log("Kerbin: " + Convert.ToString(FlightGlobals.GetBodyByName("Kerbin").transform.position) + " Kerbol: " + Convert.ToString(FlightGlobals.GetBodyByName("Sun").transform.position) + " Vessel: " + Convert.ToString(FlightGlobals.ActiveVessel.transform.position));
+            //light.intensity = (float)rnd.Next(1, 4);
+            //light.color = new Color((float)rnd.NextDouble(), (float)rnd.NextDouble(), (float)rnd.NextDouble(), 1.0F);
+            //go.transform.rotation *= Quaternion.Euler(0.0F, -90 * Time.deltaTime, 0.0F);
+
+            if (FlightGlobals.ActiveVessel)
+            {
+                CelestialBody planet = FlightGlobals.GetBodyByName("Kerbin");
+                Vector3 planetPos = planet.transform.position;
+                Vector3 sunPos = FlightGlobals.GetBodyByName("Sun").transform.position;
+                Vector3 shipPos = FlightGlobals.ActiveVessel.transform.position;
+                double r = planet.Radius;
+
+                double dShip = Math.Pythagoran;
+
+
+
+
+                Debug.Log("Kerbin: " + Convert.ToString(FlightGlobals.GetBodyByName("Kerbin").transform.position) + " Kerbol: " + Convert.ToString(FlightGlobals.GetBodyByName("Sun").transform.position) + " Vessel: " + Convert.ToString(FlightGlobals.ActiveVessel.transform.position));
+            }
         }
     }
 }
