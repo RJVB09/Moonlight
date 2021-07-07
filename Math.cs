b979175e59ccb36d52446f469bd601b1413bb45d
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2021-07-07 22:06:41 +0200

    I did stuff
    wow

--- Math.cs
+++ Math.cs
@@ -10,15 +10,6 @@ namespace Moonlight
 {
     class Math
     {
-        public static double PythagoreanV3(Vector3 v3a, Vector3 v3b) 
-        {
-            double d = Sqrt(Pow((v3a.x - v3b.x), 2) + Pow((v3a.y - v3b.y), 2) + Pow((v3a.z - v3b.z), 2));
-            return d;
-        }
-        public static double PythagoreanV2(Vector2 v3a, Vector2 v3b)
-        {
-            double d = Sqrt(Pow((v3a.x - v3b.x), 2) + Pow((v3a.y - v3b.y), 2));
-            return d;
-        }
+        
     }
 }
--- Moonlight.cs
+++ Moonlight.cs
@@ -15,23 +15,43 @@ namespace Moonlight
     [KSPAddon(KSPAddon.Startup.AllGameScenes,false)]
     public class Moonlight : MonoBehaviour
     {
-        GameObject go;
-        Light light;
         CelestialBody planet;
         System.Random rnd = new System.Random();
-
-        public static Vector3
+        string curentScene = SceneManager.GetActiveScene().name;
         public void Start()
         {
             //go = new GameObject();
             //light = go.AddComponent<Light>();
             //light.type = LightType.Directional;
-            //light.shadows = LightShadows.Hard;
+            //light.shadows = LightShadows.Hard
             //StartCoroutine(SlowUpdate());
             //go.transform.position = Vector3.zero;
 
             planet = FlightGlobals.GetBodyByName("Kerbin");
-            string curentScene = SceneManager.GetActiveScene().name;
+            if (curentScene == "trackingStation")
+            {
+                GameObject gox = new GameObject();
+                Light lightx = gox.AddComponent<Light>();
+                lightx.type = LightType.Directional;
+                lightx.shadows = LightShadows.Hard;
+                lightx.color = new Color(1, 0, 0);
+                gox.transform.rotation = Quaternion.Euler(90, 0, 0);
+
+                GameObject goy = new GameObject();
+                Light lighty = goy.AddComponent<Light>();
+                lighty.type = LightType.Directional;
+                lighty.shadows = LightShadows.Hard;
+                lighty.color = new Color(0, 1, 0);
+                goy.transform.rotation = Quaternion.Euler(0, 90, 0);
+
+                GameObject goz = new GameObject();
+                Light lightz = goz.AddComponent<Light>();
+                lightz.type = LightType.Directional;
+                lightz.shadows = LightShadows.Hard;
+                lightz.color = new Color(0, 0, 1);
+                goz.transform.rotation = Quaternion.Euler(0, 0, 90);
+            }
+            FlightGlobals.GetBodyByName("Kerbin").orbit.inclination = 90;
         }
         public IEnumerator SlowUpdate()
         { 
@@ -50,18 +70,48 @@ namespace Moonlight
             if (FlightGlobals.ActiveVessel)
             {
                 CelestialBody planet = FlightGlobals.GetBodyByName("Kerbin");
+                CelestialBody star = FlightGlobals.GetBodyByName("Sun");
+                float i = 1;
+
+
                 Vector3 planetPos = planet.transform.position;
-                Vector3 sunPos = FlightGlobals.GetBodyByName("Sun").transform.position;
+                Vector3 sunPos = star.transform.position;
                 Vector3 shipPos = FlightGlobals.ActiveVessel.transform.position;
                 double r = planet.Radius;
 
-                double dShip = Math.Pythagoran;
+                double dShip = PythagoreanV3(shipPos,planetPos);
+                double sAltitude = FlightGlobals.ActiveVessel.altitude;
+                double dSun = PythagoreanV3(sunPos, planetPos);
+                float t = (float)Max(RadToDeg(Atan(sAltitude / r)) / 90, 0);
+
+                Vector3 vShip = shipPos - planetPos;
+                Vector3 vSun = sunPos - planetPos;
+                float a = 1 - (float)RadToDeg(Acos((vShip.x * vSun.x + vShip.y * vSun.y + vShip.z * vSun.z) / (dShip * dSun)) / 180);
+
+                float l1 = (t * a) + 2 * ((1 - t) * (float)Max(a - 0.5, 0));
+                float l2 = (float)Min(Pow(0.5, sAltitude / (4f /*<--- slope*/ * r)), 1); ;
+                float lightIntensity = l1 * l2 * i;
 
 
 
-
-                Debug.Log("Kerbin: " + Convert.ToString(FlightGlobals.GetBodyByName("Kerbin").transform.position) + " Kerbol: " + Convert.ToString(FlightGlobals.GetBodyByName("Sun").transform.position) + " Vessel: " + Convert.ToString(FlightGlobals.ActiveVessel.transform.position));
+                //Debug.Log("-------------");
+                //Debug.Log("lightIntensity " + Convert.ToString(lightIntensity));
             }
+        }
+        public static double PythagoreanV3(Vector3 v3a, Vector3 v3b)
+        {
+            double d = Sqrt(Pow((v3a.x - v3b.x), 2) + Pow((v3a.y - v3b.y), 2) + Pow((v3a.z - v3b.z), 2));
+            return d;
+        }
+        public static double PythagoreanV2(Vector2 v3a, Vector2 v3b)
+        {
+            double d = Sqrt(Pow((v3a.x - v3b.x), 2) + Pow((v3a.y - v3b.y), 2));
+            return d;
+        }
+        public static double RadToDeg(double x)
+        {
+            double y = x * 180f / PI;
+            return y;
         }
     }
 }
